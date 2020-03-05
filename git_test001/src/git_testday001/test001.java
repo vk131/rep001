d9390144c6fb9354ba38361573120407543dcc14
sadfasf <adgfasf@qq.com>
2020-03-05 11:01:13 +0800

    test001

---
+++ git_test001/src/git_testday001/test001.java
@@ -0,0 +1,15 @@
+package git_testday001;
+
+/**
+ * @project_name:git_test001
+ * @author: LK
+ * @create: 2020-03-05
+ **/
+public class test001 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("this is  git_test00"+i+" ");
+        }
+    }
+}
